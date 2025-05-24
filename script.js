2d405d25eaa76841e707174995458dd5e81aec40
Jonas Rocha <jonasrpnstudy@gmail.com>
2025-05-24 14:51:07 -0300

    Capturando o evento submit no formulário

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-//Obtendo input:
-
-const amount = document.getElementById("amount");
-const currency = document.getElementById("currency");
+const form = document.querySelector("form"); //obtendo formulário
+const amount = document.getElementById("amount"); //obtendo input:
+const currency = document.getElementById("currency"); //obtendo o select das moedas
 
 // Manipulando o input "amount" para receber somente números.
 amount.addEventListener("input", () => {
   const hasCharactersRegex = /\D+/g;
   amount.value = amount.value.replace(hasCharactersRegex, "");
 });
+
+// Capturando o evento de submit no formulário
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault(event);
+  console.log(currency.value);
+});
+
+/*form.onsubmit = (event) => {
+  event.preventDefault(event)
+  console.log(event);
+};*/
